1255d2bfc551bb1f7b2d15fc99c6b32f9829b257
manjitwaikhom <manjitsinghwk@gmail.com>
2018-05-26 07:22:27 +0530

    Making DB online

--- SpringWithJDBC/src/main/java/com/spring/jdbc/JdbcCustomerDAO.java
+++ SpringWithJDBC/src/main/java/com/spring/jdbc/JdbcCustomerDAO.java
@@ -10,7 +10,7 @@ public class JdbcCustomerDAO implements CustomerDAO {
 
 	@Override
 	public void addCustomer(CustomerTO cto) {
-		String sql="insert into customers values(?,?,?,?,?)";
+		String sql="insert into CUSTOMER values(?,?,?,?,?)";
 		Object args[]= {cto.getCid(),cto.getCname(),cto.getEmail(),cto.getPhone(),cto.getCity()};
 		int i=jdbcTemp.update(sql, args);
 		System.out.println(i);
@@ -18,7 +18,7 @@ public class JdbcCustomerDAO implements CustomerDAO {
 
 	@Override
 	public void updateCustomer(CustomerTO cto) {
-		String sql="update customers set cname=?,email=?,phone=?,city=? where cid=?";
+		String sql="update CUSTOMER set cname=?,email=?,phone=?,city=? where cid=?";
 		Object args[]= {cto.getCid(),cto.getCname(),cto.getEmail(),cto.getPhone(),cto.getCity()};
 		int i=jdbcTemp.update(sql, args);
 		System.out.println(i);
@@ -27,7 +27,7 @@ public class JdbcCustomerDAO implements CustomerDAO {
 
 	@Override
 	public void deleteCustomer(int cid) {
-		String sql="delete from  customers  where cid=?";
+		String sql="delete from  CUSTOMER  where cid=?";
 		Object args[]= {cid};
 		jdbcTemp.update(sql, args);
 
---
+++ SpringWithJDBC/src/main/java/com/spring/jdbc/MYSQL.sql
@@ -0,0 +1,6 @@
+--SQL Server URL: db4free.net 
+--MySQL 8.0 database server
+--Database: manjitwaikhomdb
+--Username: manjitwaikhom
+--Email: manjitsinghwk@gmail.com
+CREATE TABLE CUSTOMER (cid INT(11) PRIMARY KEY,cname VARCHAR(30) NOT NULL,email VARCHAR(50),phone int(11),city VARCHAR(30));
